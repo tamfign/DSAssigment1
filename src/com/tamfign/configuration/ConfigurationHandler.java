5bc50a3ab812695993e657478dacafa9f29c67fa
Tamfign <tamfign@gmail.com>
2016-10-05 22:51:04 +1100

    Revert "Delete server config xml handler"
    This reverts commit ae2fc08514a73351c62d07a9b204bcfd716e97a0.

---
+++ src/com/tamfign/configuration/ConfigurationHandler.java
@@ -0,0 +1,71 @@
+package com.tamfign.configuration;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import com.tamfign.model.RouterListController;
+import com.tamfign.model.ServerListController;
+
+public class ConfigurationHandler extends DefaultHandler {
+	private static final String SERVER = "server";
+	private static final String ROUTER = "router";
+	private static final String SERVER_ID = "server_id";
+	private static final String HOST = "host";
+	private static final String CLIENT_PORT = "client_port";
+	private static final String COODINATE_PORT = "coordinate_port";
+	private static final String IS_MAIN = "is_main";
+
+	private String tag = null;
+	private ServerConfig config = null;
+
+	@Override
+	public void characters(char[] ch, int start, int length) throws SAXException {
+		String data = new String(ch, start, length);
+
+		if (SERVER_ID.equalsIgnoreCase(this.tag)) {
+			this.config.setId(data);
+		} else if (HOST.equalsIgnoreCase(this.tag)) {
+			this.config.setHost(data);
+		} else if (CLIENT_PORT.equalsIgnoreCase(this.tag)) {
+			this.config.setClientPort(Integer.parseInt(data));
+		} else if (COODINATE_PORT.equalsIgnoreCase(this.tag)) {
+			this.config.setCoordinationPort(Integer.parseInt(data));
+		} else if (IS_MAIN.equalsIgnoreCase(this.tag)) {
+			((RouterConfig) this.config).setMain(Boolean.parseBoolean(data));
+		}
+	}
+
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		if (SERVER.equals(qName)) {
+			ServerListController.getInstance().addServer(this.config);
+			this.config = null;
+		} else if (ROUTER.equals(qName)) {
+			RouterListController.getInstance().addRouter(this.config);
+			this.config = null;
+		}
+		this.tag = null;
+	}
+
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attri) throws SAXException {
+		switch (qName) {
+		case SERVER:
+			this.config = new ServerConfig();
+			break;
+		case ROUTER:
+			this.config = new RouterConfig();
+			break;
+		case SERVER_ID:
+		case HOST:
+		case CLIENT_PORT:
+		case COODINATE_PORT:
+		case IS_MAIN:
+			this.tag = qName;
+			break;
+		default:
+			this.tag = null;
+		}
+	}
+}
