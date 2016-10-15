82cf91c52b180fbe6ee97e48c5d951438e4bde02
Tamfign <tamfign@gmail.com>
2016-10-15 16:12:04 +1100

    Delete useless code in InternalCmd.java

--- src/com/mindplus/command/InternalCmd.java
+++ src/com/mindplus/command/InternalCmd.java
@@ -13,16 +13,16 @@ public class InternalCmd extends Command {
 
 	public static Command getInternRoomCmd(Command oldCmd, String cmd, String roomId) {
 		JSONObject obj = new JSONObject();
-		obj.put(Command.CMD, cmd);
-		obj.put(Command.P_ROOM_ID, roomId);
+		obj.put(CMD, cmd);
+		obj.put(P_ROOM_ID, roomId);
 		return new InternalCmd(oldCmd, obj);
 	}
 
 	public static Command getInternRoomResultCmd(Command oldCmd, String cmd, String roomId, boolean result) {
 		JSONObject obj = new JSONObject();
-		obj.put(Command.CMD, cmd);
-		obj.put(Command.P_ROOM_ID, roomId);
-		obj.put(Command.P_APPROVED, result);
+		obj.put(CMD, cmd);
+		obj.put(P_ROOM_ID, roomId);
+		obj.put(P_APPROVED, result);
 		return new InternalCmd(oldCmd, obj);
 	}
 
@@ -33,23 +33,23 @@ public class InternalCmd extends Command {
 
 	public static Command getInternIdCmd(Command oldCmd, String cmd, String identity) {
 		JSONObject obj = new JSONObject();
-		obj.put(Command.CMD, cmd);
-		obj.put(Command.P_IDENTITY, identity);
+		obj.put(CMD, cmd);
+		obj.put(P_IDENTITY, identity);
 		return new InternalCmd(oldCmd, obj);
 	}
 
 	public static Command getLockIdentityResultCmd(Command oldCmd, boolean approved) {
 		JSONObject obj = new JSONObject();
-		obj.put(Command.CMD, Command.CMD_LOCK_IDENTITY);
-		obj.put(Command.P_APPROVED, approved);
+		obj.put(CMD, CMD_LOCK_IDENTITY);
+		obj.put(P_APPROVED, approved);
 		return new InternalCmd(oldCmd, obj);
 	}
 
 	public static Command getLockRoomResultCmd(Command oldCmd, String roomId, boolean approved) {
 		JSONObject obj = new JSONObject();
-		obj.put(Command.CMD, Command.CMD_LOCK_ROOM);
-		obj.put(Command.P_ROOM_ID, roomId);
-		obj.put(Command.P_APPROVED, approved);
+		obj.put(CMD, CMD_LOCK_ROOM);
+		obj.put(P_ROOM_ID, roomId);
+		obj.put(P_APPROVED, approved);
 		return new InternalCmd(oldCmd, obj);
 	}
 
